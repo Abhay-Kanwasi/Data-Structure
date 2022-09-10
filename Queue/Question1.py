c8830461d242d42e5b7ab5e37f180f0b6ca8c8c1
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2022-09-10 14:25:08 +0530

    Question1 completed

---
+++ Queue/Question1.py
@@ -0,0 +1,56 @@
+'''
+Design a food ordering system where your python program will run two threads,
+
+Place Order: This thread will be placing an order and inserting that into a queue. This thread places new order every 0.5 second. (hint: use time.sleep(0.5) function)
+
+Serve Order: This thread will server the order. All you need to do is pop the order out of the queue and print it. This thread serves an order every 2 seconds. Also start this thread 1 second after place order thread is started.
+'''
+
+from collections import deque
+import time 
+import threading
+
+class Queue:
+    def __init__(self):
+        self.buffer = deque()
+
+    def enqueue(self,value):
+        self.buffer.appendleft(value)
+    
+    def dequeue(self):
+        if len(self.buffer)==0:
+            print("Queue is empty.")
+            return
+        return self.buffer.pop()
+    
+    def is_empty(self):
+        return len(self.buffer)==0
+
+    def size(self):
+        return len(self.buffer)
+
+food_order_queue = Queue()
+
+def place_order(orders):
+    for order in orders:
+        print(f"Placing a order for {order}.")
+        food_order_queue.enqueue(order)
+        time.sleep(0.5)
+    
+def serve_food():
+    time.sleep(1)
+    while True:
+        order = food_order_queue.dequeue()
+        print("Now serving :",order)
+        time.sleep(2)
+
+
+if __name__ == '__main__':
+    orders = ['pizza','samosa','pasta','biryani','burger']
+    t1 = threading.Thread(target=place_order,args=(orders,))
+    t2 = threading.Thread(target=serve_food)
+
+    t1.start()
+    t2.start()
+
+    
