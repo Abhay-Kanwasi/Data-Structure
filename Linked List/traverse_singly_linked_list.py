bfd27019b93927daab378908039fba1a3574f5d5
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2022-09-27 23:28:10 +0530

    name changed

---
+++ Linked List/traverse_singly_linked_list.py
@@ -0,0 +1,7 @@
+# For implementing linked list we create data and refrence node. For that these are the steps 
+class Node:
+    def __init__(self,data,ref):
+        self.data = data
+        self.ref = None
+        
+        
