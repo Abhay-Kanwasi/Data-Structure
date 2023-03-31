0c727ac6165ac125e53af9895170182962ca092b
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-03-31 23:44:06 +0530

    permutation added

---
+++ Algorithms/Algorithm 1/permutation.py
@@ -0,0 +1,13 @@
+def permute(string, pocket=" "):
+    if len(string) == 0:
+        print(pocket)
+    else:
+        for i in range(len(string)):
+            letter = string[i]
+            front = string[0:i]
+            back = string[i + 1:]
+            together = front + back
+            permute(together, letter + pocket)
+
+
+permute("AB", " ")
