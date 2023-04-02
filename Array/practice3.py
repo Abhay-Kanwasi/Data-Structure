8ab66457a5cc17f2eaac624bfeef2a09ba7127a2
Abhay Kanwasi <78997764+Abhay-Coder001@users.noreply.github.com>
2023-04-02 19:41:01 +0530

    practice 3 added

--- Array/practice3.py
+++ Array/practice3.py
@@ -3,21 +3,18 @@
 arr = [2, 3, 8, 9]
 print(f"Array before {arr}")
 
-
 # Method 1
-# arr_reverse = arr[::-1]
-# print(f"Array after {arr_reverse}")
+arr_reverse = arr[::-1]
+print(f"Array after {arr_reverse}")
 
 
 # Method 2
-n = len(arr)
-def reverse_arr(arr, start, end):
-    while start < end:
-        temp = arr[start]
-        arr[start] = arr[end]
-        arr[end] = temp
-        start += 1
-        end -= 1
-for i in range(5):
-    print(arr[i])
+reversed = []
+for value in arr:
+    reversed = [value] + reversed
+print(reversed)
+
+
+# Method 3
+
 
